c58da9b4dc21fcdddbbe854cee94cf97744fd60a
jeevithac24 <jeevithac@gmail.com>
2024-08-29 16:42:39 +0530

    message

---
+++ gun/src/com/xworkz/gun/products/Granny.java
@@ -0,0 +1,17 @@
+package com.xworkz.gun.products;
+
+public class Granny {
+	private Saree saree = new Saree("Banarasi", 7000, "blue");
+
+	public void fold() {
+		if(saree!=null) {
+		System.out.println("running fold in saree");
+		saree.enhanceLook();
+		saree.print();
+		}
+		else {
+			System.out.println("saree is null");
+		}
+	}
+
+}
---
+++ gun/src/com/xworkz/gun/runner/WashingMachineRunner.java
@@ -0,0 +1,21 @@
+package com.xworkz.gun.products;
+
+public class WashingMachineRunner {
+
+	public static void main(String[] args) {
+
+		Megha megha = new Megha();
+		megha.wash();
+		Lakshmi lakshmi = new Lakshmi();
+		WashingMachine washingMachine = new WashingMachine("Samsung", "topload", 12);
+		lakshmi.dry(washingMachine);
+		WashingMachine washingMachine1 = new WashingMachine("bosch", "frontload", 20);
+		Abhishek abhishek = new Abhishek(washingMachine1);
+		abhishek.clean();
+		Bhumika bhumika = new Bhumika();
+		bhumika.use();
+		Nayana nayana = new Nayana();
+		nayana.drumClean();
+	}
+
+}
